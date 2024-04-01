46599a510cd91469c79235ac70aa553cb3c9c913
Aamir Raza <merciful9795@gmail.com>
2024-04-01 14:27:42 +0530

    minor fix

--- backend/index.js
+++ backend/index.js
@@ -33,8 +33,8 @@ app.use(express.static(path.resolve(__dirname, "dist")));
 
  
 mongoose.connect(process.env.MONGODB_URL).then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
   });
 });
 
